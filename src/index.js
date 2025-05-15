b26a44875dc75a1e27da9ad1bb6253d370593b99
fabi-agn <fabi.agn1500@gmail.com>
2025-05-15 11:16:52 +0100

    fixed code

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ function search(event) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function formatDay(timestamp) {
@@ -74,13 +74,13 @@ function formatDay(timestamp) {
 }
 
 function getForecast(city) {
-  let apiKey = tbf3a261f4a63878c1o0bfa0c1f9d6b6;
+  let apiKey = "tbf3a261f4a63878c1o0bfa0c1f9d6b6";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
 function displayForecast(response) {
-  console.log(responde.data);
+  console.log(response.data);
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
